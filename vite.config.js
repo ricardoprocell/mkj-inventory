b05afe2cc2e0d0eee307dd265f31596fae668f58
ricardoprocell <99979561+ricardoprocell@users.noreply.github.com>
2026-08-06 19:45:52 -0600

    Add legacy plugin for iOS Safari 13+

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['ios >= 13', 'safari >= 13'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
   base: '/mkj-inventory/',
 })
